b94a779e0eaa71fdffffb01cd9545984cdb43433
Jamie Jason <125273279+scoundrellacoach@users.noreply.github.com>
2023-02-13 17:29:25 +0200

    Create Trainee lvl.js

---
+++ lesson 1/Trainee lvl.js
@@ -0,0 +1,18 @@
+define( [
+	"./core",
+	"../external/sizzle/dist/sizzle"
+], function( jQuery, Sizzle ) {
+
+"use strict";
+
+jQuery.find = Sizzle;
+jQuery.expr = Sizzle.selectors;
+};
+       {
+    svgRule
+      .oneOf('inline')
+      .resourceQuery(/inline/)
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+}
